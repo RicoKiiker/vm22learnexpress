a493ceac593ce15ce2811cd4aaa0665726dea34e
Rico Kiiker <Rico.kiiker@tptlive.ee>
2024-04-17 15:02:04 +0300

    Immigratsioon rai

---
+++ seeders/20240417105521-users-seeder.js
@@ -0,0 +1,25 @@
+'use strict';
+const{User}= require('../models/index.js');
+const bcrypt = require('bcrypt');
+const { faker } = require('@faker-js/faker');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.create({
+      name: 'Rico Kiiker',
+      email: 'Rico.kiiker@tptlive.ee',
+      password: bcrypt.hashSync('Passw0rd', 12)
+    });
+    for(let i = 0; i<10;i++){
+      await User. create({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: bcrypt.hashSync('Passw0rd', 12)
+      });
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+await queryInterface.bulkDelete('Users', null, {});
+  }
+};
